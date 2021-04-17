7ef0f2f8f96290835e5d2a5f2e125bd86ab68e48
Israe Moubayed <israetarek@yahoo.com>
2021-04-17 08:17:27 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@
 document.getElementById('create-task-form').addEventListener("DOMContentLoaded", (event) => {
   // your code here
   
-  event.submit(function(e){
-    e.preventDefault();
+  
   });
   
 });
